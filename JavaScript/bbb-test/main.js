5597942607b46fe9e3ffc20fde88ae7676b61fd8
koootyan <87225519+koootyan@users.noreply.github.com>
2022-12-12 21:33:59 +0900

    LP課題テスト
    cssにコメント
    レスポンシブ対応
    ファビコン設定

--- JavaScript/bbb-test/main.js
+++ JavaScript/bbb-test/main.js
@@ -1,3 +1,4 @@
+// ハンバーガーメニュー
 $(".openbtn1").click(function () {//ボタンがクリックされたら
     $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
       $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
@@ -8,18 +9,18 @@ $(".openbtn1").click(function () {//ボタンがクリックされたら
       $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
   });
 
-
-  var slideConts = document.querySelectorAll('.slideConts'); // スライドで表示させる要素の取得
-var slideContsRect = []; // 要素の位置を入れるための配列
-var slideContsTop = []; // 要素の位置を入れるための配列
-var windowY = window.pageYOffset; // ウィンドウのスクロール位置を取得
-var windowH = window.innerHeight; // ウィンドウの高さを取得
-var remainder = 100; // ちょっとはみ出させる部分
+// BBBが選ばれる理由スライド
+let slideConts = document.querySelectorAll('.slideConts'); // スライドで表示させる要素の取得
+let slideContsRect = []; // 要素の位置を入れるための配列
+let slideContsTop = []; // 要素の位置を入れるための配列
+let windowY = window.pageYOffset; // ウィンドウのスクロール位置を取得
+let windowH = window.innerHeight; // ウィンドウの高さを取得
+let remainder = 100; // ちょっとはみ出させる部分
 // 要素の位置を取得
-for (var i = 0; i < slideConts.length; i++) {
+for (let i = 0; i < slideConts.length; i++) {
   slideContsRect.push(slideConts[i].getBoundingClientRect());
 }
-for (var i = 0; i < slideContsRect.length; i++) {
+for (let i = 0; i < slideContsRect.length; i++) {
   slideContsTop.push(slideContsRect[i].top + windowY);
 }
 // ウィンドウがリサイズされたら、ウィンドウの高さを再取得
@@ -28,22 +29,22 @@ window.addEventListener('resize', function () {
 });
 // スクロールされたら
 window.addEventListener('scroll', function () {
-  // スクロール位置を取得
-  windowY = window.pageYOffset;
+// スクロール位置を取得
+windowY = window.pageYOffset;
   
   for (var i = 0; i < slideConts.length; i++) {
-    // 要素が画面の下端にかかったら
+  // 要素が画面の下端にかかったら
     if(windowY > slideContsTop[i] - windowH + remainder) {
-      // .showを付与
+    // .showを付与
       slideConts[i].classList.add('show');
     } else {
-      // 逆に.showを削除
-      slideConts[i].classList.remove('show');
+    // 逆に.showを削除
+    slideConts[i].classList.remove('show');
     }
   }
 });
 
-
+// 受講生の声ポップアップ表示
 window.addEventListener("load", function() {
     const target = document.querySelectorAll('.scr-target')
     const targetArray = Array.prototype.slice.call(target);
@@ -66,4 +67,4 @@ window.addEventListener("load", function() {
         }
       });
     };
-    });
+});
